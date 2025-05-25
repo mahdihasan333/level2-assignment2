03756579e5c66bc59af2ca2b38d681084081a674
Mahdi Hassan <mahdioffi333@gmail.com>
2025-05-25 18:11:18 +0600

    create 3 table

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -5,5 +5,25 @@ CREATE Table rangers(
     ranger_id SERIAL PRIMARY KEY NOT NULL,
     name VARCHAR(50) NOT NULL,
     region VARCHAR(50) NOT NULL
-)
+);
 
+
+CREATE type thread_status AS ENUM ('Endangered', 'Vulnerable');
+
+CREATE TABLE species(
+    species_id SERIAL PRIMARY KEY NOT NULL,
+    common_name VARCHAR(50) NOT NULL,
+    scientific_name VARCHAR(50),
+    discovery_date DATE NOT NULL,
+    conservation_status thread_status NOT NULL
+);
+
+
+CREATE Table sightings(
+    sighting_id SERIAL PRIMARY KEY NOT NULL,
+    ranger_id INTEGER NOT NULL REFERENCES rangers(ranger_id),
+    species_id INTEGER NOT NULL REFERENCES species(species_id),
+    sighting_time TIMESTAMP WITHOUT TIME ZONE,
+    location VARCHAR(50) NOT NULL,
+    notes TEXT
+)
