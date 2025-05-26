47aecf5bde014f2f213530d92a3680f744cee8b1
Mahdi Hassan <mahdioffi333@gmail.com>
2025-05-26 14:27:36 +0600

    problem 8 solved : Label each sightings time of day as Morning, Afternoon, or Evening

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -103,6 +103,15 @@ WHERE discovery_date < '1800-01-01';
 
 
 
+-- Problem Number 8 : Label each sighting's time of day as 'Morning', 'Afternoon', or 'Evening'.
+SELECT sighting_id, CASE
+WHEN EXTRACT (HOUR FROM sighting_time) < 12 THEN 'Morning'
+WHEN EXTRACT (HOUR FROM sighting_time) BETWEEN 12 AND 17 THEN 'Afternoon'
+ELSE 'Evening' END AS time_of_day FROM sightings;
+
+
+
+
 SELECT * FROM rangers;
 SELECT * FROM species;
 SELECT * FROM sightings;
